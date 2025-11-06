c4a8da7d817894da0d4f75c8637275ccc52d6560
Sergio Barreras <sergiobg.isc@gmail.com>
2025-11-05 22:29:36 -0600

    feat: add environment configurations and integrate ngx-toastr for notifications

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'localhost:3000/v1',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://api.botbite.com.mx/v1',
+};
